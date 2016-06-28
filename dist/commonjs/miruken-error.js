54588911aaec47292d4c61c8b5fcf1c32136e95c
Craig Neuwirt <cneuwirt@gmail.com>
2016-06-28 10:50:28 -0500

    Added dist

---
+++ dist/commonjs/miruken-error.js
@@ -0,0 +1,67 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.ErrorCallbackHandler = exports.Errors = undefined;
+
+var _mirukenCore = require('miruken-core');
+
+var _mirukenCallback = require('miruken-callback');
+
+var Errors = exports.Errors = _mirukenCore.Protocol.extend({
+    handleError: function handleError(error, context) {},
+    handleException: function handleException(exception, context) {},
+    reportError: function reportError(error, context) {},
+    reportException: function reportException(exception, context) {},
+    clearErrors: function clearErrors(context) {}
+});
+
+var ErrorCallbackHandler = exports.ErrorCallbackHandler = _mirukenCallback.CallbackHandler.extend(Errors, {
+    handleError: function handleError(error, context) {
+        var result = Errors(_mirukenCallback.$composer).reportError(error, context);
+        return result === undefined ? Promise.reject(error) : Promise.resolve(result);
+    },
+    handleException: function handleException(exception, context) {
+        var result = Errors(_mirukenCallback.$composer).reportException(exception, context);
+        return result === undefined ? Promise.reject(exception) : Promise.resolve(result);
+    },
+    reportError: function reportError(error, context) {
+        console.error(error);
+        return Promise.resolve();
+    },
+    reportException: function reportException(exception, context) {
+        console.error(exception);
+        return Promise.resolve();
+    },
+    clearErrors: function clearErrors(context) {}
+});
+
+_mirukenCallback.CallbackHandler.implement({
+    $recover: function $recover(context) {
+        return this.filter(function (callback, composer, proceed) {
+            try {
+                var handled = proceed();
+                if (handled) {
+                    var result = callback.callbackResult;
+                    if ((0, _mirukenCore.$isPromise)(result)) {
+                        callback.callbackResult = result.catch(function (err) {
+                            return Errors(composer).handleError(err, context);
+                        });
+                    }
+                }
+                return handled;
+            } catch (ex) {
+                Errors(composer).handleException(ex, context);
+                return true;
+            }
+        });
+    },
+    $recoverError: function $recoverError(context) {
+        var _this = this;
+
+        return function (error) {
+            return Errors(_this).handleError(error, context);
+        };
+    }
+});
---
+++ dist/miruken-error.js
@@ -0,0 +1,114 @@
+import {Protocol,$isPromise} from 'miruken-core';
+import {CallbackHandler,$composer} from 'miruken-callback';
+
+/**
+ * Protocol for handling and reporting errors.
+ * @class Errors
+ * @extends miruken.Protocol
+ */    
+export const Errors = Protocol.extend({
+    /**
+     * Handles an error.
+     * @method handlerError
+     * @param   {Any}          error      - error (usually Error)
+     * @param   {Any}          [context]  - scope of error
+     * @returns {Promise} promise of handled error.
+     */        
+    handleError(error, context) {},
+    /**
+     * Handles an exception.
+     * @method handlerException
+     * @param   {Exception}    excption   - exception
+     * @param   {Any}          [context]  - scope of error
+     * @returns {Promise} of handled error.
+     */        
+    handleException(exception, context) {},
+    /**
+     * Reports an error.
+     * @method reportError
+     * @param   {Any}          error      - error (usually Error)
+     * @param   {Any}          [context]  - scope of error
+     * @returns {Promise} of reported error.
+     */        
+    reportError(error, context) {},
+    /**
+     * Reports an excepion.
+     * @method reportException
+     * @param   {Exception}    exception  - exception
+     * @param   {Any}          [context]  - scope of exception
+     * @returns {Promise} of reported exception.
+     */        
+    reportException(exception, context) {},
+    /**
+     * Clears any errors for the associated context.
+     * @method clearErrors
+     * @param   {Any}          [context]  - scope of errors
+     */
+    clearErrors(context) {}
+});
+
+/**
+ * CallbackHandler for handling errors.
+ * @class ErrorCallbackHandler
+ * @extends miruken.callback.CallbackHandler
+ * @uses miruken.error.Errors
+ */    
+export const ErrorCallbackHandler = CallbackHandler.extend(Errors, {
+    handleError(error, context) {
+        const result = Errors($composer).reportError(error, context);
+        return result === undefined
+             ? Promise.reject(error)
+             : Promise.resolve(result);
+    },
+    handleException(exception, context) {
+        const result = Errors($composer).reportException(exception, context);
+        return result === undefined
+             ? Promise.reject(exception)
+             : Promise.resolve(result);
+    },                                                      
+    reportError(error, context) {
+        console.error(error);
+        return Promise.resolve();
+    },
+    reportException(exception, context) {
+        console.error(exception);
+        return Promise.resolve();
+    },
+    clearErrors(context) {} 
+});
+
+CallbackHandler.implement({
+    /**
+     * Marks the callback handler for recovery.
+     * @method $recover
+     * @returns {miruken.callback.CallbackHandlerFilter} recovery semantics.
+     * @for miruken.callback.CallbackHandler
+     */        
+    $recover(context) {
+        return this.filter((callback, composer, proceed) => {
+            try {
+                const handled = proceed();
+                if (handled) {
+                    const result = callback.callbackResult;
+                    if ($isPromise(result)) {
+                        callback.callbackResult = result.catch(err =>
+                            Errors(composer).handleError(err, context));
+                    }
+                }
+                return handled;
+            } catch (ex) {
+                Errors(composer).handleException(ex, context);
+                return true;
+            }
+        });
+    },
+    /**
+     * Creates a function to pass error promises to Errors feature.
+     * @method $recoverError
+     * @returns {Function} function to pass error promises to Errors feature. 
+     * @for miruken.callback.CallbackHandler
+     */        
+    $recoverError(context) {
+        return error => Errors(this).handleError(error, context);
+    }
+});
